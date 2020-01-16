eb1a606469448c45827a192b19a66f077b62e660
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2020-01-16 09:28:34 -0300

    Initial commit

---
+++ BasicPlatform/Game1.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BasicPlatform.Desktop
+{
+    public class Game1 : Game
+    {
+        //Display
+        const int SCREEN_WIDTH = 320;
+        const int SCREEN_HEIGHT = 180;
+        const bool FULLSCREEN = false;
+
+        GraphicsDeviceManager graphics;
+        PresentationParameters pp;
+        SpriteBatch spriteBatch;
+
+        static public int screenW, screenH;
+        static public Vector2 screen_center;
+        Rectangle screenRect, desktopRect;
+        float speed = 50;
+
+        // Textures
+        Texture2D far_background, mid_background;
+        Texture2D tiles_images;
+
+        //POSITIONS
+        static public Vector2 background_pos;
+
+        //RenderTargets
+        RenderTarget2D MainTarget;
+
+        // INPUT
+        Input input;
+
+        public Game1()
+        {
+            int initial_screen_width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 10;
+            int initial_screen_height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 10;
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = initial_screen_width,
+                PreferredBackBufferHeight = initial_screen_height,
+                IsFullScreen = FULLSCREEN,
+                PreferredDepthStencilFormat = DepthFormat.Depth16,
+            };
+            Window.IsBorderless = true;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            MainTarget = new RenderTarget2D(GraphicsDevice, SCREEN_WIDTH, SCREEN_HEIGHT);
+            pp = GraphicsDevice.PresentationParameters;
+            SurfaceFormat format = pp.BackBufferFormat;
+            screenW = MainTarget.Width;
+            screenH = MainTarget.Height;
+            desktopRect = new Rectangle(0, 0, pp.BackBufferWidth, pp.BackBufferHeight);
+            screenRect = new Rectangle(0, 0, screenW, screenH);
+            screen_center = new Vector2(screenW / 2f, screenH / 2f) - new Vector2(32f, 32f);
+
+            input = new Input();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            far_background = Content.Load<Texture2D>("background_stars");
+            mid_background = Content.Load<Texture2D>("mid_background");
+            tiles_images = Content.Load<Texture2D>("tileset");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            input.Update();
+            if (input.Keypress(Keys.Escape)) Exit();
+            if (input.Keydown(Keys.Left)) background_pos.X += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (input.Keydown(Keys.Right)) background_pos.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (input.Keydown(Keys.Up)) background_pos.Y -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (input.Keydown(Keys.Down)) background_pos.Y += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.SetRenderTarget(MainTarget);
+
+            // DRAW STUFF
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, SamplerState.PointWrap);
+            spriteBatch.Draw(far_background, screenRect, new Rectangle((int)(-background_pos.X * 0.5f), 0, far_background.Width, far_background.Height), Color.White);
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, SamplerState.PointWrap);
+            spriteBatch.Draw(mid_background, screenRect, new Rectangle((int)(-background_pos.X), (int)(-background_pos.Y), far_background.Width, far_background.Height), Color.White);
+            spriteBatch.End();
+
+            // DRAW MAINTARGET TO BACKBUFFER
+            GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, SamplerState.PointWrap, DepthStencilState.None, RasterizerState.CullNone);
+            spriteBatch.Draw(MainTarget, desktopRect, Color.White);
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ BasicPlatform/UserInterface/Input.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework.Input;
+
+namespace BasicPlatform.Desktop
+{
+    public class Input
+    {
+        public KeyboardState kb, okb;
+        public bool shift_down, control_down, alt_down;
+        public bool shift_press, control_press, alt_press;
+        public bool old_shift_down, old_control_down, old_alt_down;
+
+        public Input()
+        {
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Keypress(Keys k)
+        {
+            return kb.IsKeyDown(k) && okb.IsKeyUp(k);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Keydown(Keys k)
+        {
+            return kb.IsKeyDown(k);
+        }
+
+        public void Update()
+        {
+            old_alt_down = alt_down;
+            old_shift_down = shift_down;
+            old_control_down = control_down;
+            okb = kb;
+            kb = Keyboard.GetState();
+            shift_down = kb.IsKeyDown(Keys.LeftShift) || kb.IsKeyDown(Keys.RightShift);
+            control_down = kb.IsKeyDown(Keys.LeftControl) || kb.IsKeyDown(Keys.RightControl);
+            alt_down = kb.IsKeyDown(Keys.LeftAlt) || kb.IsKeyDown(Keys.RightAlt);
+            shift_press = shift_down && !old_shift_down;
+            alt_press = alt_down && !old_alt_down;
+            control_press = control_press && !old_control_down;
+        }
+    }
+}
